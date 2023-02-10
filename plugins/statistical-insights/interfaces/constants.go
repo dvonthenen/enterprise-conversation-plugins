6f5617a2e31b18a23923138b06f4c90a9b3fea30
dvonthenen <david.vonthenen@gmail.com>
2023-02-09 18:52:38 -0800

    Initial Implementation Stats Plugin

---
+++ plugins/statistical-insights/interfaces/constants.go
@@ -0,0 +1,17 @@
+// Copyright 2023 Symbl.ai SDK contributors. All Rights Reserved.
+// SPDX-License-Identifier: Apache License 2.0
+
+package interfaces
+
+const (
+	// app specific message type
+	AppSpecificMessageTypeStatistical string = "statistical"
+
+	// user/app level statistical type
+	UserStatisticalTypeTopic   string = "topic_statistical"
+	UserStatisticalTypeTracker string = "tracker_statistical"
+	UserStatisticalTypeEntity  string = "entity_statistical"
+
+	// app specific message type
+	MessageNotFound string = "**MESSAGE NOT FOUND**"
+)
